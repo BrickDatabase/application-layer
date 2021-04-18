932132114d17f27634c6e01d2f76093542a75fce
Joel Borden <joel.parker714@outlook.com>
2021-04-17 23:52:55 -0400

    add ed's pythons

---
+++ src/services/baseSQL.py
@@ -0,0 +1,44 @@
+##############################################
+# Author:                                    #
+# Edward Riley                               #
+##############################################
+
+import psycopg2
+
+try:
+    databaseVar = psycopg2.connect("dbname=subreddit_db")
+    databaseVar.autocommit = True
+    databaseVar.set_session(autocommit=True)
+
+
+except:
+    print("Database Connection Failed")
+    exit(2)
+
+
+def selectAllInformation():
+    mycursor = databaseVar.cursor()
+    mycursor.execute("SELECT * FROM information")
+    myresult = mycursor.fetchall()
+    for x in myresult:
+        print(x)
+
+def returnSelectAllAbbreviation():
+    mycursor = databaseVar.cursor()
+    mycursor.execute("SELECT * FROM lookup")
+    myresult = mycursor.fetchall()
+    return myresult
+
+def insertRowInformation(id, date, subscribers, activeSubscribers, submission, comment):
+    mycursor = databaseVar.cursor()
+    mycursor.execute("INSERT INTO information (subreddit_id, date, subscribers, active_subscribers, submission, comments) VALUES ('" + str(id) + "', '"+ str(date) + "', '" + str(subscribers) + "', '" + str(activeSubscribers) + "', '" + str(submission) + "', '" + str(comment) + "');")
+
+def insertSubreddit(fullname, subreddit):
+    tupleVar = (fullname, subreddit)
+    mycursor = databaseVar.cursor()
+    mycursor.execute("INSERT INTO lookup (name, abbreviation) VALUES(%s, %s)", tupleVar)
+
+def deleteSubreddit(id):
+    mycursor = databaseVar.cursor()
+    mycursor.execute("DELETE from lookup WHERE id = " + str(id) + ";")
+    mycursor.execute("DELETE from information WHERE subreddit_id = " + str(id) + ";")
---
+++ src/services/manual_get_call.py
@@ -0,0 +1,72 @@
+##############################################
+# Author:                                    #
+# Edward Riley                               #
+##############################################
+
+try:
+    import baseAPI
+except:
+    print("baseAPI.py file not found. ")
+    exit(1)
+
+try:
+    import baseSQL
+except:
+    print("baseSQL.py file not found.")
+    exit(1)
+
+try:
+    import apscheduler
+except: 
+    print("apscheduler package not found.")
+
+try:
+    import numpy
+except:
+    print("Numpy package not found.")
+    exit(1)
+
+try:
+    import json
+except:
+    print("JSON package not found.")
+    exit(1)
+
+try:
+    from datetime import datetime
+    import time
+except:
+    print("Datetime or Time not found.")
+    exit(1)
+
+subredditArray = baseSQL.returnSelectAllAbbreviation()
+
+subredditID = 1
+print(subredditArray)
+
+for subreddit in subredditArray:
+    
+    subreddit = numpy.asarray(subreddit)
+
+    # API endpoint will crash if you go too fast, after some tests, 1 second is the optimal speed. 
+    time.sleep(1)
+    print(subredditID)
+    
+    url = "/r/" + subreddit[2] + "/about/"
+    
+    result = (baseAPI.getResult(url))
+    submission = baseAPI.getSubmissionResult(subreddit[2])['metadata']['total_results']
+    comment = baseAPI.getCommentResult(subreddit[2])['metadata']['total_results']
+
+    #print(subreddit.capitalize() + " Subscribers:\t" + str(result['data']['subscribers']))
+    #print(subreddit.capitalize() + " Active Users Count:\t" + str(result['data']['active_user_count']))
+    #print(subreddit.capitalize() + " Submission:\t" + str(submission))
+    #print(subreddit.capitalize() + " Comment:\t" + str(comment))
+
+    subscribers = result['data']['subscribers']
+    activeSubscribers = result['data']['active_user_count']
+    date=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    baseSQL.insertRowInformation(subredditID, date, subscribers, activeSubscribers, submission, comment)
+    subredditID = subredditID + 1
+
+baseSQL.selectAllInformation()
